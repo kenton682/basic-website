c19f3fde5bf2819d263598fc66407226e3463f0b
Kenton Olsen <kenton682@gmail.com>
2020-09-27 19:15:43 -0700

    Baic-website template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Pages/Home';
+import Services from './components/Pages/Services'
+import Products from './components/Pages/Products'
+import SignUp from './components/Pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/Services' component={Services}/>
+        <Route path='/Products' component={Products}/>
+        <Route path='/sign-up' component={SignUp}/>
+      </Switch>
+    </Router>
+      
+    </>
   );
 }
 
